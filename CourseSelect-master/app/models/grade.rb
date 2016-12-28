b79cc87f8d453ebb8f2eb31f3c53a2b223a4745c
ranshunag <yyyyyhua@gmail.com>
2016-12-28 10:07:16 +0800

    修复分数的验证，学生功能选项等bug

--- CourseSelect-master/app/models/grade.rb
+++ CourseSelect-master/app/models/grade.rb
@@ -2,5 +2,5 @@ class Grade < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
 
-  validates :grade, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: '分数必须在0到100之间的整数' }
+  validates_numericality_of :grade, allow_nil: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: '分数必须在0到100之间的数'
 end
